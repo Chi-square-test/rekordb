84625d8684c519c81cebd75f90de069252129e24
chi-square-test <luckykr1234@gmail.com>
2022-08-02 22:30:46 +0900

    Initial commit

---
+++ src/main/java/com/rekordb/rekordb/RekordbApplication.java
@@ -0,0 +1,13 @@
+package com.rekordb.rekordb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekordbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekordbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rekordb/rekordb/RekordbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rekordb.rekordb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekordbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

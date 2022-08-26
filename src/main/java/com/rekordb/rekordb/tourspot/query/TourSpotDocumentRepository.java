faf029f49e9db043b08cdf06c2a5907a2bcfd50a
Chi-square-test <luckykr1234@gmail.com>
2022-08-26 16:57:11 +0900

    태그 관련 몽고db 이전 및 spot 데이터 이전 절차

--- src/main/java/com/rekordb/rekordb/tourspot/query/TourSpotDocumentRepository.java
+++ src/main/java/com/rekordb/rekordb/tourspot/query/TourSpotDocumentRepository.java
@@ -3,6 +3,8 @@ package com.rekordb.rekordb.tourspot.query;
 import com.rekordb.rekordb.tourspot.domain.SpotId;
 import com.rekordb.rekordb.tourspot.domain.TourSpotDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TourSpotDocumentRepository extends MongoRepository<TourSpotDocument, SpotId> {
 }

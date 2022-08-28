01e830eee6586f643fe66f8808e75566d60bc01f
Chi-square-test <luckykr1234@gmail.com>
2022-08-28 13:23:30 +0900

    회원가입 검증부분 추가

--- src/main/java/com/rekordb/rekordb/user/dto/RekorJoinInformDTO.java
+++ src/main/java/com/rekordb/rekordb/user/dto/RekorJoinInformDTO.java
@@ -20,11 +20,11 @@ public class RekorJoinInformDTO {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birth;
     @NotNull(message = "성별정보")
-    private int gender; //여자 0 남자 1
+    private Integer gender; //여자 0 남자 1
     @NotNull(message = "국가정보")
     private String country;
     @NotNull(message = "언어정보")
-    private int language; //한글 0 영어 1
+    private Integer language; //한글 0 영어 1
 
 
 }

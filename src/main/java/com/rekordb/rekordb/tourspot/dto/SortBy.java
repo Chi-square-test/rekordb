1a88804c157735d12d6c1f1931d7c4e059553369
Chi-square-test <luckykr1234@gmail.com>
2022-08-29 00:07:39 +0900

    오타수정

--- src/main/java/com/rekordb/rekordb/tourspot/dto/SortBy.java
+++ src/main/java/com/rekordb/rekordb/tourspot/dto/SortBy.java
@@ -7,16 +7,29 @@ import java.util.Arrays;
 
 @AllArgsConstructor
 public enum SortBy {
-    title("Name") ,
-    likcCount("Likes"),
-    rating("Rates");
+    title("Name") {
+        @Override
+        public Sort getSort() {
+            return Sort.by(Sort.Direction.ASC, this.name());
+        }
+    } ,
+    likcCount("Likes"){
+        @Override
+        public Sort getSort() {
+            return Sort.by(Sort.Direction.DESC, this.name());
+        }
+    },
+    rating("Rates"){
+        @Override
+        public Sort getSort() {
+            return Sort.by(Sort.Direction.DESC, this.name());
+        }
+    };
 
 
     private String front;
 
-    public Sort getSort() {
-        return Sort.by(Sort.Direction.ASC, this.name());
-    }
+    public abstract Sort getSort();
 
     public static SortBy getByFront(String name){
         return Arrays.stream(values())

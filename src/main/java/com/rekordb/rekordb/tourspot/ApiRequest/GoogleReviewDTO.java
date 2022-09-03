71b90a65abf20ba0b9acb20c688da6959b269678
Chi-square-test <luckykr1234@gmail.com>
2022-09-04 02:02:12 +0900

    ReKor API Version 1.0.4D ----------------------- Summary : 리뷰 테스트

--- src/main/java/com/rekordb/rekordb/tourspot/ApiRequest/GoogleReviewDTO.java
+++ src/main/java/com/rekordb/rekordb/tourspot/ApiRequest/GoogleReviewDTO.java
@@ -1,10 +1,15 @@
 package com.rekordb.rekordb.tourspot.ApiRequest;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,10 +27,24 @@ public class GoogleReviewDTO implements Serializable {
     @Getter
     @ToString
     public static class review implements Serializable{
-        int rating;
-        String relative_time_description;
-        long time;
-        String text;
+        @JsonProperty("author_name")
+        private String authorName;
+        @JsonProperty("author_url")
+        private String authorUrl;
+        @JsonProperty("language")
+        private String language;
+        @JsonProperty("profile_photo_url")
+        private String profilePhotoUrl;
+        @JsonProperty("rating")
+        private Integer rating;
+        @JsonProperty("relative_time_description")
+        private String relativeTimeDescription;
+        @JsonProperty("text")
+        private String text;
+        @JsonProperty("time")
+        private Integer time;
+        @JsonIgnore
+        private Map<String, Object> additionalProperties = new HashMap<String, Object>();
     }
 
 }
